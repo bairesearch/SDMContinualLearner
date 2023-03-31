9c67a124373f9f4760fcbb100ccb75bf3b5a2d66
Richard Baxter <richard.baxter@baxterai.com>
2023-03-31 16:42:48 +1100

    update models/__init__.py - disable nta/htmpapers/biorxiv/going_beyond_the_point_neuron nupic dependencies (requires ray 0.8.3), resolves ERROR: Could not find a version that satisfies the requirement ray[tune]~=0.8.3 (from nupic-research-ray)

--- models/__init__.py
+++ models/__init__.py
@@ -5,5 +5,5 @@ from .CLASSIC_FFN import CLASSIC_FFN
 from .Conv_Mixer import ConvMixer
 from .TopK_Act import Top_K
 from .SDM_Base import SDMBase
-from .Interface_Active_Dendrites import InterfActiveDendrites
+#from .Interface_Active_Dendrites import InterfActiveDendrites
 from .Conv_SDM import ConvSDM
